4d36739c00ea3e5adc440725e5efcae413dd4ffb
David Lejeune <Lejeune.David@outlook.com>
2016-09-26 09:14:08 +0200

    basic setup with lib folder

---
+++ app.rb
@@ -0,0 +1,5 @@
+#!/usr/bin/env ruby
+
+require './lib/weight_converter.rb'
+
+weight_converter = WeightConverter.new
---
+++ lib/weight_converter.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+
+class WeightConverter
+
+
+
+
+
+
+
+end
